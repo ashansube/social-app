1e31013c2559c5fa7bb4666bbe5cf2395d7b6f37
@ashan_IT19063560 <ashan.sql@gmail.com>
2023-01-28 21:46:23 +0530

    docker composed project

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-
+  MONGOURI: "mongodb://mongodb:27017/docker-db",
+    // "mongodb+srv://ashan:QQLBL6KSoqxniei7@cluster0.eiv6dvx.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
 };
